951de99567cb146ff54970e2419fc77efb91bad7
Alexander Summa <info@alexandersumma.de>
2024-10-24 19:40:05 +0200

    Implement controls to enable cycling through content while the modal is open

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,24 +1,80 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const openModal = (tile) => {
+    const setModalContent = (tile) => {
         const modalTitle = tile.querySelector('h2')?.textContent || 'Lala';
         const modalContent = tile.querySelector('.modal-content') || 'Lorem ipsum dolor sit amet.';
-        const modalBody = document.querySelector('#modal-1-text');
 
         document.getElementById('modal-1-title').textContent = modalTitle;
-        modalBody.innerHTML = modalContent.innerHTML;
+        document.querySelector('#modal-1-text').innerHTML = modalContent.innerHTML;
+
+        setModalControls(tile);
+    }
+
+    const setModalControls = (tile) => {
+        if (!getPreviousTile(tile)) {
+            document.querySelector('.modal__control--prev').classList.add('hidden');
+        } else {
+            document.querySelector('.modal__control--prev').classList.remove('hidden');
+        }
+
+        if (!getNextTile(tile)) {
+            document.querySelector('.modal__control--next').classList.add('hidden');
+        } else {
+            document.querySelector('.modal__control--next').classList.remove('hidden');
+        }
+    }
+
+    const openModal = (tile) => {
+        setModalContent(tile);
 
         const modalOptions = {
             disableFocus: true,
             disableScroll: true,
+            openClass: 'visible',
+            onClose: () => {
+                tile.classList.remove('active');
+            }
         }
         MicroModal.show('modal-1', modalOptions);
     }
 
-    // Prevent tap from being propagated to elements behind the modal
-    const modalOverlay = document.querySelector('.modal__overlay');
+    const changeModalContent = (direction) => {
+        const currentTile = document.querySelector('.grid-item.active');
 
-    modalOverlay.addEventListener('touchstart', (e) => {
+        let newTile;
+        if (direction === 'next') {
+            newTile = getNextTile(currentTile);
+        } else if (direction === 'prev') {
+            newTile = getPreviousTile(currentTile);
+        }
+
+        if (newTile) {
+            setModalContent(newTile);
+
+            // Update the active tile
+            currentTile.classList.remove('active');
+            newTile.classList.add('active');
+        }
+    }
+
+    const getPreviousTile = (tile) => {
+        let previousTile = tile.previousElementSibling;
+        while (previousTile && !previousTile.querySelector('.modal-content')) {
+            previousTile = previousTile.previousElementSibling;
+        }
+        return previousTile;
+    }
+
+    const getNextTile = (tile) => {
+        let nextTile = tile.nextElementSibling;
+        while (nextTile && !nextTile.querySelector('.modal-content')) {
+            nextTile = nextTile.nextElementSibling;
+        }
+        return nextTile;
+    }
+
+    // Prevent tap from being propagated to elements behind the modal
+    document.querySelector('.modal__overlay').addEventListener('touchstart', (e) => {
         if (e.target.hasAttribute('data-micromodal-close')) {
             e.preventDefault();
         }
@@ -27,7 +83,19 @@ document.addEventListener('DOMContentLoaded', () => {
     // Fill the modal before opening it with the contents of the tile
     document.querySelector('.grid-container').addEventListener('click', (event) => {
         const tile = event.target.closest('.grid-item:has(.modal-content)');
-        if (tile) openModal(tile);
+        if (tile) {
+            openModal(tile);
+            tile.classList.add('active');
+        }
+    });
+
+    // Add event listeners to the controls
+    document.querySelector('.modal__control--prev').addEventListener('click', function() {
+        changeModalContent('prev');
+    });
+
+    document.querySelector('.modal__control--next').addEventListener('click', function() {
+        changeModalContent('next');
     });
 
 });
