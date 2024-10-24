23b40de554ee78acc5e444b93f6ee58319a1aafd
Alexander Summa <info@alexandersumma.de>
2024-10-24 12:40:11 +0200

    Modularise using a dedicated function for opening the modal

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,6 +5,17 @@ MicroModal.init({
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    const openModal = (tile) => {
+        const modalTitle = tile.querySelector('h2')?.textContent || 'Lala';
+        const modalContent = tile.querySelector('.modal-content') || 'Lorem ipsum dolor sit amet.';
+        const modalBody = document.querySelector('#modal-1-text');
+
+        document.getElementById('modal-1-title').textContent = modalTitle;
+        modalBody.innerHTML = modalContent.innerHTML;
+
+        MicroModal.show('modal-1');
+    }
+
     // Prevent tap from being propagated to elements behind the modal
     const modalOverlay = document.querySelector('.modal__overlay');
 
@@ -14,22 +25,10 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }, { passive: false });
 
-
     // Fill the modal before opening it with the contents of the tile
-    const tiles = document.querySelectorAll('.grid-item:has(.modal-content)');
-
-    tiles.forEach(tile => {
-        tile.addEventListener('click', () => {
-            const modalContent = tile.querySelector('.modal-content');
-            const modalTitle = tile.querySelector('h2')?.textContent || 'Lala';
-
-            // Copy the entire .modal-content markup into the modal body
-            const modalBody = document.querySelector('#modal-1-text');
-            modalBody.innerHTML = modalContent.innerHTML;
-
-            document.getElementById('modal-1-title').textContent = modalTitle;
-
-            MicroModal.show('modal-1');
-        });
+    document.querySelector('.grid-container').addEventListener('click', (event) => {
+        const tile = event.target.closest('.grid-item:has(.modal-content)');
+        if (tile) openModal(tile);
     });
+
 });
